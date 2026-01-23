c49fe285253bf041698375dafc8125766788845e
ma3llim007 <mohdsameer68257@gmail.com>
2026-01-23 22:31:57 +0530

    feat: setup prettier, eslitnt and tsconfig

---
+++ 11_typescritp_project/cli_notes/eslint.config.js
@@ -0,0 +1,29 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+];
