26e4c6e595c57bd1e934e5839b296b4425917d99
ma3llim007 <mohdsameer68257@gmail.com>
2026-02-01 14:39:26 +0530

    feat: add profressional logger

---
+++ 11_typescritp_project/taskflow/src/utils/logger.ts
@@ -0,0 +1,17 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, printf, colorize, errors } = format;
+
+// Custom log format
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+    return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+const logger = createLogger({
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    format: combine(colorize(), timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), errors({ stack: true }), logFormat),
+    transports: [new transports.Console(), new transports.File({ filename: "logs/error.log", level: "error" }), new transports.File({ filename: "logs/combined.log" })],
+    exitOnError: false,
+});
+
+export default logger;
